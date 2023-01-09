720b857ebd1b396d51756b955faa1e86940a681b
djurakulov14 <109537648+djurakulov14@users.noreply.github.com>
2023-01-09 18:27:02 +0500

    finished project

---
+++ src/Contexts/spotifycontext.js
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+const spotify = createContext({
+    client_id: "6a42aacaee9c4cc0ae30355af62c5593",
+    REDIRECT_URI: "http://localhost:3000",
+    AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
+    RESPONSE_TYPE: "token",
+    token: ""
+})
+
+export default spotify
